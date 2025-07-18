4f358d2aaf87b8293473e098a08f82cc9479708f
XxAdamxX28 <126411222+MRlLIONGG@users.noreply.github.com>
2025-07-18 17:53:13 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function goToLogin() {
-  alert("Go to login (this would change screen in full version).");
-}
-
-function goToRegister() {
-  alert("Go to register (this would change screen in full version).");
-}
