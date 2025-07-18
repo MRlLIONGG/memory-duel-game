6f9d660b4367d30317f1416b0e32db548c29ae52
XxAdamxX28 <126411222+MRlLIONGG@users.noreply.github.com>
2025-07-18 17:46:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function goToLogin() {
+  alert("Go to login (this would change screen in full version).");
+}
+
+function goToRegister() {
+  alert("Go to register (this would change screen in full version).");
+}
